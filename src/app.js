742976cdc836274dcaa97c934a2101c772693380
Matt Neill <matt@poetretail.com>
2020-08-15 23:17:27 -0400

    fixed prod bug

--- src/app.js
+++ src/app.js
@@ -7,27 +7,7 @@ import express from './services/express'
 import api from './api'
 
 const app = express(apiRoot, api)
-
-let server;
-
-// function requireHTTPS(req, res, next) {
-//   // The 'x-forwarded-proto' check is for Heroku
-//   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-//     return res.redirect('https://' + req.get('host') + req.url);
-//   }
-//   next();
-// }
-
-if (process.env.NODE_ENV === 'production') {
-  const httpsOptions = {
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem')
-  }
-  server = https.createServer(httpsOptions, app);
-}
-else {
-  server = http.createServer(app);
-}
+const server = http.createServer(app);
 
 if (mongo.uri) {
   mongoose.connect(mongo.uri)
